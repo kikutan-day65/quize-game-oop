e999af9dfb2de6e5db3f41dfebd508ab9a9665a4
Haruto Mori <baystarsman@outlook.jp>
2023-03-21 18:01:30 +0100

    add function to calculate the score

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
-    question_text = question['text']
-    question_anwer = question['answer']
-    new_question = Question(question_text, question_anwer)
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-quiz.next_q
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
